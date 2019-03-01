8cf0d396b565e9fbe1447fe0d2f2d67d67527d81
uchikuch <ucheazinge@gmail.com>
2019-03-01 10:33:09 +0100

    Add GET /todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 
+const { ObjectID } = require("mongodb");
 let { mongoose } = require("./db/mongoose");
 let { Todo } = require("./models/todo");
 let { User } = require("./models/user");
@@ -34,6 +35,35 @@ app.get("/todos", (req, res) => {
       res.status(400).send(e);
     }
   );
+});
+
+app.get("/todos/:id", (req, res) => {
+  let id = req.params.id;
+
+  // validate id using is valid
+  // 404 - send back empty send
+  if (!ObjectID.isValid(id)) {
+    console.log("id not valid");
+    res.status(404).send();
+  }
+
+  // findById
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        res.status(404).send();
+        console.log("Todo Not Found");
+      } else {
+        console.log("TodoByID", todo);
+        res.status(200).send({ todo });
+      }
+    })
+    .catch(e => res.status(400).send());
+  // success
+  // if todo - send back
+  // if no todo - send back 404 with empty body
+  // error
+  // 400 - send empty body back
 });
 
 app.listen(3000, () => {
